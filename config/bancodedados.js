00b961f7745110c9ba80055f8a3f0639d726fa96
glitchy-luiz <84513178+glitchy-luiz@users.noreply.github.com>
2022-03-16 07:52:28 -0300

    Add files via upload

--- config/bancodedados.js
+++ config/bancodedados.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const conexao = async() => {
-    var atlas = await mongoose.connect('meu link')
+    var atlas = await mongoose.connect('mongodb+srv://userAdmin:S!mple4lu@fiaptecnico.yqfxr.mongodb.net/test')
 }
 
 module.exports
